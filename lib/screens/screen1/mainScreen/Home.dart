193f2e06a765a7ebacdfd50e8b7f661320362109
mohamedkhrouf <mohamed.khrouf@esprit.tn>
2021-06-24 09:59:28 +0100

    commit-

---
+++ lib/screens/screen1/mainScreen/Home.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+class Cont extends StatelessWidget {
+  Container x;
+
+  Cont()
+      : x = Container(
+    color: Colors.blue,
+    height: 200.0,
+    width: 300.0,
+    margin: EdgeInsets.all(16),
+    child: Text("Hello"),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    x = Container(
+      color: Colors.blue,
+      height: 200.0,
+      width: 300.0,
+      margin: EdgeInsets.all(16),
+      child: Text("Hello"),
+    );
+    return x;
+  }
+}
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var container = Container(
+    color: Colors.blue,
+    height: 200.0,
+    width: double.infinity,
+    margin: EdgeInsets.all(16),
+    child: Text("Hello"),
+  );
+  var tabs = [
+    SingleChildScrollView(
+      child: Column(
+        children: [Cont(), Cont(), Cont(), Cont()],
+      ),
+    ),
+    Center(
+      child: Text("Groups"),
+    ),
+    Center(
+      child: Text("Camp sites"),
+    ) ,
+    Center(
+      child: Text("Tasks"),
+    ),
+    Center(
+      child: Text("Profile"),
+    )
+  ];
+  var _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext buildContext) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text("hello l fou9"),
+        backgroundColor: Color.fromRGBO(50, 60, 70, 1),
+      ),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          backgroundColor: Color.fromRGBO(50, 60, 70, 1),
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text("Home"),
+                ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.group),
+                title: Text("My events"),
+                backgroundColor: Colors.white24),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.map),
+                title: Text("Camp sites"),
+                backgroundColor: Colors.white24),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.map),
+                title: Text("My tasks"),
+                backgroundColor: Colors.white24),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                title: Text("Profile"),
+                backgroundColor: Colors.white24)
+          ],
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          }),
+    );
+  }
+}
