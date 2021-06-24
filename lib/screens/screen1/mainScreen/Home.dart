77d921864516d0ae984e7b70a3656d4a73ff40c1
mohamedkhrouf <mohamed.khrouf@esprit.tn>
2021-06-24 11:29:23 +0100

    button

--- lib/screens/screen1/mainScreen/Home.dart
+++ lib/screens/screen1/mainScreen/Home.dart
@@ -44,11 +44,26 @@ class _HomeState extends State<Home> {
         children: [Cont(), Cont(), Cont(), Cont()],
       ),
     ),
-    Center(
-      child: Text("Groups"),
+    Container(
+      margin: EdgeInsets.only(top: 40.0,),
+
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        OutlinedButton(
+          onPressed: () {
+            print('Received click');
+          },
+          child: const Text('Click Me'),
+        ),
+        ElevatedButton(
+          onPressed: null,
+          child: Text('button'),
+        ),
+      ]),
     ),
     Center(
-      child: Text("Camp sites"),
+      child: Text("Camp "),
     ),
     Center(
       child: Text("Tasks"),
@@ -65,47 +80,46 @@ class _HomeState extends State<Home> {
       body: tabs[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
           currentIndex: _currentIndex,
-          
           items: [
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.home,
-                color: Color.fromRGBO(170,215,62,1),
+                color: Color.fromRGBO(170, 215, 62, 1),
               ),
               label: "Home",
-              backgroundColor: Color.fromRGBO(36,34,47,1),
+              backgroundColor: Color.fromRGBO(36, 34, 47, 1),
             ),
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.group,
-                color: Color.fromRGBO(170,215,62,1),
+                color: Color.fromRGBO(170, 215, 62, 1),
               ),
               label: "My events",
-              backgroundColor: Color.fromRGBO(36,34,47,1),
+              backgroundColor: Color.fromRGBO(36, 34, 47, 1),
             ),
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.map,
-                color: Color.fromRGBO(170,215,62,1),
+                color: Color.fromRGBO(170, 215, 62, 1),
               ),
               label: "Camp sites",
-              backgroundColor: Color.fromRGBO(36,34,47,1),
+              backgroundColor: Color.fromRGBO(36, 34, 47, 1),
             ),
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.map,
-                color: Color.fromRGBO(170,215,62,1),
+                color: Color.fromRGBO(170, 215, 62, 1),
               ),
               label: "My tasks",
-              backgroundColor: Color.fromRGBO(36,34,47,1),
+              backgroundColor: Color.fromRGBO(36, 34, 47, 1),
             ),
             BottomNavigationBarItem(
               icon: Icon(
                 Icons.person,
-                color: Color.fromRGBO(170,215,62,1),
+                color: Color.fromRGBO(170, 215, 62, 1),
               ),
               label: "Profile",
-              backgroundColor: Color.fromRGBO(36,34,47,1),
+              backgroundColor: Color.fromRGBO(36, 34, 47, 1),
             )
           ],
           onTap: (index) {
