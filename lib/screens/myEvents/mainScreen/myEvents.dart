02290e75d71519694334d499c0d6c595ac0c88cc
mekkiAh <mekkiahmed123@gmail.com>
2021-06-25 16:32:47 +0100

    events

--- lib/screens/myEvents/mainScreen/myEvents.dart
+++ lib/screens/myEvents/mainScreen/myEvents.dart
@@ -1,52 +1,87 @@
 import 'package:flutter/cupertino.dart';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_camp/screens/myEvents/widgets/event.dart';
 
-class MyEvents extends StatelessWidget {
+class GroupModel {
+  GroupModel({this.groupName, this.date});
+
+  final String groupName;
+  final String date;
+}
+
+List<GroupModel> allGroups = [
+  new GroupModel(groupName: "groupName 1 .....", date: "yyyy/mm/dd"),
+  new GroupModel(groupName: "groupName2 .....", date: "yyyy/mm/dd"),
+  new GroupModel(groupName: "groupName3 .....", date: "yyyy/mm/dd"),
+  new GroupModel(groupName: "hhhhh4 .....", date: "yyyy/mm/dd"),
+  new GroupModel(groupName: "gggghhh75 .....", date: "yyyy/mm/dd"),new GroupModel(groupName: "groupName3 .....", date: "yyyy/mm/dd"),
+  new GroupModel(groupName: "hhhhh4 .....", date: "yyyy/mm/dd"),
+  new GroupModel(groupName: "gggghhh75 .....", date: "yyyy/mm/dd"),new GroupModel(groupName: "groupName3 .....", date: "yyyy/mm/dd"),
+  new GroupModel(groupName: "hhhhh4 .....", date: "yyyy/mm/dd"),
+  new GroupModel(groupName: "gggghhh75 .....", date: "yyyy/mm/dd"),new GroupModel(groupName: "groupName3 .....", date: "yyyy/mm/dd"),
+  new GroupModel(groupName: "hhhhh4 .....", date: "yyyy/mm/dd"),
+  new GroupModel(groupName: "gggghhh75 .....", date: "yyyy/mm/dd"),
+];
+class MyEvents extends StatefulWidget{
+  @override
+  _MyEvents createState() => _MyEvents();
+}
+class _MyEvents extends State<MyEvents> {
+  var groupList = allGroups;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final border = OutlineInputBorder(
       borderRadius: BorderRadius.all(Radius.circular(90.0)),
       borderSide: BorderSide(
         color: Colors.transparent,
-      )
-  );
+      ));
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-     child: Column(children: [
+    return Scaffold(
+        appBar: AppBar(
+            backgroundColor: Color.fromRGBO(36, 34, 47, 1),
+            title :Center(child: Text("My events",style: TextStyle(
+              color: Color.fromRGBO(170, 215, 62, 1)
+            ),)
+            )),
+        body: SingleChildScrollView(child:Column(
+        children: [
       Container(
-          child: Form(
-              key: _formKey,
-              child: Container(
-                  margin: EdgeInsets.all(16.0),
-                  child:
-                    TextFormField(
-                      decoration: const InputDecoration(
-                      hintText: 'Enter event name',
-                      contentPadding: EdgeInsets.only(left: 15.0),
-                      border:  OutlineInputBorder(
-                        borderRadius: BorderRadius.all( Radius.circular(90.0))
-                      ),
-                      prefixIcon: Icon(Icons.search)
+        child: Form(
+            key: _formKey,
+            child: Container(
+                margin: EdgeInsets.all(16.0),
+                child: TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: 'Enter event name',
+                        contentPadding: EdgeInsets.only(left: 15.0),
+                        border: OutlineInputBorder(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(90.0))),
+                        prefixIcon: Icon(Icons.search)),
+                    onChanged: (text) {
+                      print(text);
+                      print(groupList.length);
+                      setState(() {
+                        groupList = allGroups
+                            .where((g) => g.groupName.contains(text))
+                            .toList();
+                      });
 
-                    )
-              )
-
-              )
-          ),
-       /* decoration: BoxDecoration(
-
-            borderRadius: BorderRadius.all(Radius.circular(90.0)),
-            border: Border.all()
-
-        ),*/
-
+                    }))),
       ),
-      Group(),
-      Group(),
-      Group(),
-    ]));
+      Container(
+      child : ListView.builder(
+          shrinkWrap: true,
+          itemCount: groupList.length,
+          itemBuilder: (context, index) {
+            return Group(
+                groupName: groupList[index].groupName,
+                date: groupList[index].date);
+          }
+      )
+      )
+    ])));
   }
 }
--- lib/screens/myEvents/widgets/event.dart
+++ lib/screens/myEvents/widgets/event.dart
@@ -2,7 +2,18 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_camp/screens/discussion/mainScreen/discussion.dart';
 
-class Group extends StatelessWidget {
+class Group extends StatefulWidget {
+  final String groupName;
+
+  final String date;
+
+  const Group({Key key, this.groupName, this.date}) : super(key: key);
+
+  @override
+  _Group createState() => _Group();
+}
+
+class _Group extends State<Group> {
   @override
   Widget build(BuildContext context) {
     return new GestureDetector(
@@ -15,7 +26,6 @@ class Group extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(16.0),
           child: Row(
-
             children: [
               Container(
                 child: Container(
@@ -31,21 +41,21 @@ class Group extends StatelessWidget {
                   child: Column(
                     children: [
                       Container(
-                          child: Text("esm l groupe",
+                          child: Text(widget.groupName,
                               style: TextStyle(fontSize: 20))),
                       Container(
-                          child: Text("yyyy/mm/dd",
-                          style: TextStyle(fontSize: 15)
-                          ),
-                          margin: EdgeInsets.only(right: 30.0) ,
+                        child:
+                            Text(widget.date, style: TextStyle(fontSize: 15)),
+                        margin: EdgeInsets.only(right: 30.0),
                       ),
                     ],
                   )),
               Expanded(
                   child: Align(
-                child: Text("Pending..."),
-                alignment: Alignment.bottomRight,
-              ))
+                      child: Text("Pending..."),
+                      alignment: Alignment.bottomRight,
+                  )
+              )
             ],
           ),
         ));
