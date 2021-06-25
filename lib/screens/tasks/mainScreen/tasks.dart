b58084116a917dcc6840b9b89c101fb4d6181e88
mekkiAh <mekkiahmed123@gmail.com>
2021-06-25 12:16:55 +0100

    paasing data in params

--- lib/screens/tasks/mainScreen/tasks.dart
+++ lib/screens/tasks/mainScreen/tasks.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:my_camp/screens/tasks/widgets/takeTaskButton.dart';
+import 'package:my_camp/screens/tasks/widgets/task.dart';
 import 'package:my_camp/screens/tasks/widgets/tasksAppBar.dart';
-class Task{
-  Task({this.taskName,  this.state});
+class TaskModel{
+  TaskModel({this.taskName,  this.state});
   final String taskName;
   final bool state ;
 }
@@ -11,17 +12,17 @@ class Tasks extends StatefulWidget {
   @override
   _Tasks createState() => _Tasks();
 }
-List<Task> AllTasks = [
-  new Task(taskName: "task 1 .....",state: true),
-  new Task(taskName: "task 2 .....",state: true),
-  new Task(taskName: "task 3 .....",state: true),
-  new Task(taskName: "task 4 .....",state: true),
-  new Task(taskName: "task 75 .....",state: true),
-  new Task(taskName: "task 8 .....",state: true),
-  new Task(taskName: "task 7 .....",state: true),
-  new Task(taskName: "task 9 .....",state: true),
-  new Task(taskName: "task 10 .....",state: true),
-  new Task(taskName: "task 11 .....",state: true),
+List<TaskModel> allTasks = [
+  new TaskModel(taskName: "task 1 .....",state: true),
+  new TaskModel(taskName: "task 2 .....",state: true),
+  new TaskModel(taskName: "task 3 .....",state: true),
+  new TaskModel(taskName: "task 4 .....",state: true),
+  new TaskModel(taskName: "task 75 .....",state: true),
+  new TaskModel(taskName: "task 8 .....",state: true),
+  new TaskModel(taskName: "task 7 .....",state: true),
+  new TaskModel(taskName: "task 9 .....",state: true),
+  new TaskModel(taskName: "task 10 .....",state: true),
+  new TaskModel(taskName: "task 11 .....",state: true),
 
 ];
 class _Tasks extends State<Tasks> {
@@ -33,20 +34,11 @@ class _Tasks extends State<Tasks> {
         child: TasksAppBar(),
         preferredSize: const Size.fromHeight(100),
       ),
-      body: Center(
+      body: Container(
           child: ListView.builder(
-              itemCount: AllTasks.length,
+              itemCount: allTasks.length,
               itemBuilder: (context,index){
-                return ListTile(
-                    leading: Icon(Icons.task),
-                    title: Row(
-                        children :[
-                          Text('${AllTasks[index].taskName}'),
-                          Spacer(),
-                          TakeTaskButton(),
-                        ]
-                    )
-                );
+                return Task(taskName : allTasks[index].taskName);
               }
           )
       ),
---
+++ lib/screens/tasks/widgets/task.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:my_camp/screens/tasks/widgets/takeTaskButton.dart';
+
+class Task extends StatefulWidget{
+  final String taskName ;
+
+  const Task({Key key, this.taskName}) : super(key: key);
+
+  @override
+  _Task createState() => _Task();
+}
+class _Task extends State<Task>{
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return ListTile(
+        leading: Icon(Icons.task),
+        title: Row(
+            children :[
+              Text(widget.taskName),
+              Spacer(),
+              TakeTaskButton(),
+            ]
+        )
+    );
+  }
+}
