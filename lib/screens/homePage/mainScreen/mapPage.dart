8b86053d2efa9c2e2faa7f457b5f16913d954749
mohamedkhrouf <mohamed.khrouf@esprit.tn>
2021-06-29 15:36:56 +0100

    commit

--- lib/screens/homePage/mainScreen/mapPage.dart
+++ lib/screens/homePage/mainScreen/mapPage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import "package:latlong2/latlong.dart";
+import "package:latlong/latlong.dart";
 
 class MapPage extends StatefulWidget {
   @override
