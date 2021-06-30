40a82fbbdfc1d58b140b51f80a3c2f3d1ea6e700
mohamedkhrouf <mohamed.khrouf@esprit.tn>
2021-06-30 16:50:30 +0100

    ok

--- lib/screens/homePage/widgets/publication.dart
+++ lib/screens/homePage/widgets/publication.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
 class Cont extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -8,109 +9,101 @@ class Cont extends StatelessWidget {
       children: [
         Container(
             color: Color.fromRGBO(188, 224, 253, 1),
-            padding: EdgeInsets.only(top:16.0,bottom: 16.0),
+            padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
             width: MediaQuery.of(context).size.width,
             margin: EdgeInsets.only(top: 16),
             child: Row(
-
               children: [
-                Spacer(),
-               
-                 Container(
-                  child: CircleAvatar(
-                    radius: 30.0,
-                    backgroundImage:
-                    AssetImage("assets/mekki.jpg"),
-                    backgroundColor: Colors.transparent,
-                  ),
-                 ),
-
                 Spacer(),
                 Container(
-
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          child: Text(
-                            "name surname",
-                            style: TextStyle(fontSize: 20),
-                            textAlign: TextAlign.left,
-                          ),
-                          margin: EdgeInsets.only(right: 10),
-                        ),
-                        Container(
-                          child: Text(
-                            "name surname",
-                            style: TextStyle(fontSize: 15),
-
-                          ),
-
-                        ),
-                      ],
-                    )
+                  child: CircleAvatar(
+                    radius: 30.0,
+                    backgroundImage: AssetImage("assets/mekki.jpg"),
+                    backgroundColor: Colors.transparent,
+                  ),
                 ),
-                    Spacer(),
-                     Spacer(),
-                      Spacer(),
-                       Spacer(),
-                      Spacer(),
-                       Spacer(),
- Spacer(),
-                  Spacer(),
+                Spacer(),
+                Container(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        "name surname",
+                        style: TextStyle(fontSize: 20),
+                        textAlign: TextAlign.left,
+                      ),
+                      margin: EdgeInsets.only(right: 10),
+                    ),
+                    Container(
+                      child: Text(
+                        "name surname",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ),
+                  ],
+                )),
+                Spacer(),
+                Spacer(),
+                Spacer(),
+                Spacer(),
+                Spacer(),
+                Spacer(),
+                Spacer(),
+                Spacer(),
               ],
             )),
-         Container(
-            color: Colors.blue,
-            height: MediaQuery.of(context).size.width * 0.744,
-            width: MediaQuery.of(context).size.width,
-            child: Stack(
-              children: [
-                Image.asset(
-                  'assets/mekki.jpg',
-                  height: MediaQuery.of(context).size.width,
-                  width: MediaQuery.of(context).size.width,
-                  fit: BoxFit.fitWidth,
-                ),],),),
+        Container(
+          color: Colors.blue,
+          height: MediaQuery.of(context).size.width * 0.744,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/mekki.jpg',
+                height: MediaQuery.of(context).size.width,
+                width: MediaQuery.of(context).size.width,
+                fit: BoxFit.fitWidth,
+              ),
+            ],
+          ),
+        ),
         Container(
             color: Color.fromRGBO(255, 255, 255, 1),
-        
             width: MediaQuery.of(context).size.width,
             margin: EdgeInsets.only(bottom: 16),
             child: Column(
               children: [
-                Container(child:
-                Row(
-                  children: [
-                   Container(child:
-                    Icon(
-                      Icons.favorite,
-                      color: Color.fromRGBO(2, 255, 255, 1),
-
-                    ),
-                    margin:EdgeInsets.only(right:20)),
-                    Container(child:
-                    Icon(
-                      Icons.add_comment,
-                      color: Color.fromRGBO(2, 255, 255, 1),
-                    ),)
-                  ],
-                ),
-                margin: EdgeInsets.only(left: 10,top:10),
-                ),
-               Container(
-                          child: Text(
-                            "name surname lorem ipsum lorem ipsum lorem ipsum lorem lorem  ipsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaum lorem",
-                            style: TextStyle(fontSize: 20),
-                            textAlign: TextAlign.left,
+                Container(
+                  child: Row(
+                    children: [
+                      Container(
+                          child: Icon(
+                            FontAwesomeIcons.fire,
+                            color: Color.fromRGBO(0,0,0, 1),
                           ),
-                        
-                          margin: EdgeInsets.only(left: 10,right: 10),
+                          margin: EdgeInsets.only(right: 20)),
+                      Container(
+                        child: Icon(
+                          FontAwesomeIcons.commentDots,
+                          color: Color.fromRGBO(0,0,0, 1),
                         ),
-                  ],
-                )
-           )
+                      )
+                    ],
+                  ),
+                  margin: EdgeInsets.only(left: 10, top: 10),
+                ),
+                Container(
+                  child: Text(
+                    "name surname lorem ipsum lorem ipsum lorem ipsum lorem lorem  ipsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaum lorem",
+                    style: TextStyle(fontSize: 20),
+                    textAlign: TextAlign.left,
+                  ),
+                  margin: EdgeInsets.only(left: 10, right: 10),
+                ),
+              ],
+            ))
       ],
     );
   }
